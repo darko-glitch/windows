ba6c9759d883c1783f2cb20cc8adf3dc105aa5d6
darko-glitch <ahmadalrajo11@gmail.com>
2025-06-05 09:44:22 +0200

    add wsl

--- before.ps1
+++ before.ps1
@@ -12,3 +12,9 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 # Install pyenv 
 Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/pyenv-win/pyenv-win/master/pyenv-win/install-pyenv-win.ps1" -OutFile "./install-pyenv-win.ps1"; &"./install-pyenv-win.ps1"
 
+# ==============================================================================
+# STEP 3: Enable Windows Subsystem for Linux 
+# ==============================================================================
+# Enable Windows Subsystem for Linux 
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
